7338eb966240cf7b710534765d4736f3c9b8f565
Dragomir Dimitrov <info@nortiena.com>
2017-02-05 12:35:25 +0200

    revert test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    BG Spider
+                    BG Spiders
                 </div>
 
                 <div class="links">
