f6b24fd9efe0e3c28a5730dcd01a37f499af27d9
vagrant <vagrant@homestead>
2017-02-23 07:44:31 +0000

    Authorization middleware

---
+++ app/Http/Middleware/RedirectIfNotAuthorized.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RedirectIfNotAuthorized
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(!\Auth::user()->hasRole('admin') && !\Auth::user()->hasRole('supervisor')){
+            return redirect('/home')->with('msg', 'You are not authorized!');
+        }
+        
+        return $next($request);
+    }
+}
