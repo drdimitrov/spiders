910b2ed687eb71044b8224eac2b2473fbfe4304e
vagrant <vagrant@homestead>
2017-02-23 08:50:35 +0000

    front page setup

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,98 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/harpactea.jpg') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Sticky Footer Navbar Template for Bootstrap</title>
+@extends('layouts.main')
 
+@section('content')
+<div class="container">
+  <div class="page-header">
+    <h1>Bulgarian spiders</h1>
+  </div>
 
-  </head>
+  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?</p>
 
-  <body>
+  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae blanditiis unde libero minus qui nulla alias mollitia, sunt temporibus aperiam expedita aspernatur dolor eveniet modi beatae suscipit molestias dolore tempora.</p>
 
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
-    <!-- Begin page content -->
-    <div class="container">
-      <div class="page-header">
-        <h1>Harpactea</h1>
-      </div>
-
-      <p class="lead">
-      	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?
-      </p>
-      
-    </div>
-
-    <footer class="footer">
-      <div class="container">
-        <p class="text-muted">Footer content here.</p>
-      </div>
-    </footer>
-
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script>
-      /*!
-       * IE10 viewport hack for Surface/desktop Windows 8 bug
-       * Copyright 2014-2015 Twitter, Inc.
-       * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-       */
-
-      // See the Getting Started docs for more information:
-      // http://getbootstrap.com/getting-started/#support-ie10-width
-
-      (function () {
-        'use strict';
-
-        if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-          var msViewportStyle = document.createElement('style')
-          msViewportStyle.appendChild(
-            document.createTextNode(
-              '@-ms-viewport{width:auto!important}'
-            )
-          )
-          document.querySelector('head').appendChild(msViewportStyle)
-        }
-
-      })();
-    </script>
-  </body>
-</html>
+  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?</p>
+  
+</div>
+@endsection
