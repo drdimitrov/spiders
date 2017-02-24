1a82e35d0186b3a00ed870565eb21cf662825e9d
vagrant <vagrant@homestead>
2017-02-24 09:42:05 +0000

    algolia setup

--- app/Author.php
+++ app/Author.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Author extends Model
 {
+	use Searchable;
+	
     protected $fillable = ['last_name', 'first_name'];
 }
---
+++ resources/assets/js/components/search/Authors.vue
@@ -0,0 +1,25 @@
+<template>
+    <form action="#" @submit.prevent="show" class="form-inline">
+        <div class="form-group fg1">
+            <label for="authors">Choose Author</label>            
+            <input type="text" name="authors" id="authors" class="form-control">          
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Search</button>
+        </div>
+    </form>
+    bla
+</template>
+
+<script>
+    export default {
+        methods: {
+            show(){
+                console.log('test');
+            }
+        },
+        mounted() {
+            //
+        }
+    }
+</script>
