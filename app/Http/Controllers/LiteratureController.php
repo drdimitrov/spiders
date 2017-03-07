465859e3f02b1651894b25677fd2616fbfc51473
vagrant <vagrant@homestead>
2017-03-07 09:13:06 +0000

    Single authors papers

--- app/Http/Controllers/LiteratureController.php
+++ app/Http/Controllers/LiteratureController.php
@@ -4,23 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Paper;
+use App\Author;
 
 class LiteratureController extends Controller
 {
     public function index(Request $request){
 
-    	// $literature = Paper::with(['authors' => function($query){
-    	// 	$query->orderBy('author_paper.id');
-    	// }])->orderBy('published_at')->paginate(20);
-
-    	// if($request->has('author')){
-    	// 	$literature = 
-    	// }
-
     	$literature = Paper::with(['authors' => function($query){
-    		$query->whereIn($request->author, );
     		$query->orderBy('author_paper.id');
     	}])->orderBy('published_at')->paginate(20);
+
+    	if($request->has('author')){
+    		$singleAuth = Author::with('papers')->where('id', $request->author)->paginate(20);
+    		dd($singleAuth);
+    	}
+
 
     	
     	return view('front.literature', compact('literature'));
