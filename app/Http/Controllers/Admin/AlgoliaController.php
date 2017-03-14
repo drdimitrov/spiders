771d40c1b6f2c28ec75953adab221281d9970205
vagrant <vagrant@homestead>
2017-03-14 15:28:24 +0000

    Family model and migration

--- app/Http/Controllers/Admin/AlgoliaController.php
+++ app/Http/Controllers/Admin/AlgoliaController.php
@@ -11,7 +11,9 @@ class AlgoliaController extends Controller
     public function selectPaper(Request $request){
 
     	return Author::with(['papers' => function($query) use($request){
-    		$query->where('published_at', $request->year.'-01-01 00:00:01');
+    		if($request->has('year')){
+    			$query->where('published_at', $request->year.'-01-01 00:00:01');
+    		}    		
     	}])->find($request->author);
     }
 }
---
+++ database/migrations/2017_03_14_152229_create_families_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFamiliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('families', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('paper_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('paper_id')
+              ->references('id')->on('papers')
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('families');
+    }
+}
