b039896bdc565d5a94ade6f2a0032bce5e19f5bb
vagrant <vagrant@homestead>
2017-03-15 13:54:00 +0000

    send mail functionality

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\EnquirySend;
 
 class FrontController extends Controller
 {
@@ -15,6 +16,9 @@ class FrontController extends Controller
     }
 
     public function postContact(Request $request){
+
+    	\Mail::to('info@nortiena.com')->send(new EnquirySend($request));
+
     	return redirect(route('contact'))->withMsg('Thank you for contacting us. We will get back to you as soon as possible.');
     }
 }
