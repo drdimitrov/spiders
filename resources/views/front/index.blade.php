910f023b360d0e71fb036d0ec9576d3cd209d38b
vagrant <vagrant@homestead>
2017-03-30 12:11:32 +0000

    single species view

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -11,6 +11,6 @@
   <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae blanditiis unde libero minus qui nulla alias mollitia, sunt temporibus aperiam expedita aspernatur dolor eveniet modi beatae suscipit molestias dolore tempora.</p>
 
   <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?</p>
-  
+    
 </div>
 @endsection
--- resources/views/front/single-species.blade.php
+++ resources/views/front/single-species.blade.php
@@ -1,1 +1,12 @@
-{{ $species->genus->name }} {{ $species->name }}
+@extends('layouts.main')
+
+@section('content')
+@php($cnt = 1)
+<div class="container">
+	<div class="page-header">
+		<h1>{{ $species->genus->name }} {{ $species->name }}</h1>
+	</div>
+
+      
+</div>
+@endsection
