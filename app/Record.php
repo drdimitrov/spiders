3524dc82c3bd845b465e444dfdd0979bf46538ce
vagrant <vagrant@homestead>
2017-04-04 08:33:33 +0000

    Record model

---
+++ app/Record.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Record extends Model
+{
+    protected $fillable = [
+	    'species_id', 
+	    'locality_id', 
+	    'comments',
+	    'males',
+	    'females',
+	    'juvenile_males',
+	    'juvenile_females',
+	    'collected_by',
+	    'collected_at',
+    ];
+}
