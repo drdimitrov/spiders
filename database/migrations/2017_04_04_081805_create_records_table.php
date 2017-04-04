1708028dd22161dcdf8bea3b256173296c9afaa1
vagrant <vagrant@homestead>
2017-04-04 08:29:32 +0000

    records migration

---
+++ database/migrations/2017_04_04_081805_create_records_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('species_id')->unsigned()->index();
+            $table->foreign('species_id')->references('id')->on('species')->onDelete('cascade');
+            $table->integer('locality_id')->unsigned()->index();
+            $table->foreign('locality_id')->references('id')->on('localities')->onDelete('cascade');
+            $table->longText('comments');
+            $table->integer('males');
+            $table->integer('females');
+            $table->integer('juvenile_males');
+            $table->integer('juvenile_females');
+            $table->string('collected_by');
+            $table->timestamp('collected_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
