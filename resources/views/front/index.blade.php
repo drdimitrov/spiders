39f24fce258df1282ea64472b36765f0bcacb53c
vagrant <vagrant@homestead>
2017-04-04 18:24:42 +0000

    test

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
   <div class="page-header">
-    <h1>Bulgarian spiders</h1>
+    <h1>*Bulgarian spiders</h1>
   </div>
 
   <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ratione, doloremque dolore eos, iusto, magnam dolorem illo sint nesciunt molestiae nulla rem nihil veniam. Libero exercitationem dolore molestiae, similique quis?</p>
