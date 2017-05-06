44318df73091864df4d5e3bd1e1cc33ca04deb5b
vagrant <vagrant@homestead>
2017-05-06 14:43:42 +0000

    maps

--- resources/views/front/single-species.blade.php
+++ resources/views/front/single-species.blade.php
@@ -34,7 +34,7 @@
     	@endforeach
     </div>
     <div class="col-md-6">
-    	<div id="map" style="width: 400px; height: 500px;"></div>
+    	<div id="map" style="width: 500px; height: 450px;"></div>
     </div>  
 </div>
 
