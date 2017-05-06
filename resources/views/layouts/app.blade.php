296b7e2f026ff42b4d27ffdfbd801b76b4c195c2
vagrant <vagrant@homestead>
2017-05-06 15:26:25 +0000

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -170,7 +170,7 @@
                       </div>
                     </nav>
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-10">
                 @else
                 <div class="col-md-12">
                 @endif
