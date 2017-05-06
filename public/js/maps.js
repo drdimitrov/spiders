ecadcca811c67a9b1fe4772db390ec08f01f2d94
vagrant <vagrant@homestead>
2017-05-06 13:30:30 +0000

    maps

--- public/js/maps.js
+++ public/js/maps.js
@@ -7,4 +7,13 @@ initMap = function(){
       scrollwheel: false,
       zoom: 8
     });
+
+    var marker = new google.maps.Marker({
+    position: {
+      	lat: 42.131470, 
+      	lng: 27.750139
+      },,
+    map: map,
+    title: 'Tsarevo'
+  });
 }
