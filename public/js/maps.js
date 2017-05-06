0dee556f7a502d2796a67abff527f842cd2469cb
vagrant <vagrant@homestead>
2017-05-06 13:22:53 +0000

    maps

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,8 +1,8 @@
 initMap = function(){
 	var map = new google.maps.Map(document.getElementById('map'), {
       center: {
-      	lat: -34.397, 
-      	lng: 150.644
+      	lat: 42.131470, 
+      	lng: 27.750139
       },
       scrollwheel: false,
       zoom: 8
