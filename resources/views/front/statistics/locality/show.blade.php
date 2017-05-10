f2278c8a0aa8888ec6e1473029ad67bc3cfabfb1
vagrant <vagrant@homestead>
2017-05-10 07:35:58 +0000

    species by locality fix

--- resources/views/front/statistics/locality/show.blade.php
+++ resources/views/front/statistics/locality/show.blade.php
@@ -16,18 +16,18 @@
           </tr>
         </thead>
         <tbody>
-            @foreach($spLocs as $v)
+            @foreach($spLocs as $spK => $v)
               @foreach($v as $spec => $loc)
                   <tr>
                       <td>{{ $cnt }}</td>
-                      <td>{{ $spec }}</td>
+                      <td><a href="/species/{{ $spK }}">{{ $spec }}</a></td>
                       <td>
                         @foreach($loc as $l)
                           @if(isset($l['date'])) {{ $l['date'] }}, @endif
                           @if(isset($l['males'])) {{ $l['males'] }}  &#9794;, @endif
                           @if(isset($l['females'])) {{ $l['females'] }}  &#9792;, @endif
-                          @if(isset($l['juvenile_males'])) {{ $l['juvenile_males'] }}  &#9794;, @endif
-                          @if(isset($l['juvenile_females'])) {{ $l['juvenile_females'] }}  &#9792;, @endif
+                          @if(isset($l['juvenile_males'])) {{ $l['juvenile_males'] }} juv. &#9794;, @endif
+                          @if(isset($l['juvenile_females'])) {{ $l['juvenile_females'] }} juv. &#9792;, @endif
                           @if(isset($l['collected_by'])) {{ $l['collected_by'] }} leg., @endif
                           (<a href="/literature/{{$l['paper_slug']}}">{{ $l['paper'] }}</a>)<br>
                         @endforeach
