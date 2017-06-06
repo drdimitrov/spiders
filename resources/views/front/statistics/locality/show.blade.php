e8cd7dd576f4cfc792cb3eb5e13be093a4a5fd2b
vagrant <vagrant@homestead>
2017-06-06 14:45:50 +0000

    total species in locality

--- resources/views/front/statistics/locality/show.blade.php
+++ resources/views/front/statistics/locality/show.blade.php
@@ -4,7 +4,11 @@
 @php($cnt = 1)
 <div class="container">
      <div class="page-header">
-        <h1>{{ $lName }} - list of species</h1>        
+        <h1>
+        {{ $lName }} - list of species 
+        <span style="font-size: 0.6em; color: #8c8c8c;">
+        (total {{ count($spLocs) }})
+        </span></h1>        
       </div>
 
       <a href="/statistics/species-by-locality" class="btn btn-primary pull-right">Back to localities</a>
