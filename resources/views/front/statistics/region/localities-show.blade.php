d9ec6836cdad3e670938b7bb5eb45b94a0691a8c
vagrant <vagrant@homestead>
2017-06-06 15:41:44 +0000

    localities by region map

--- resources/views/front/statistics/region/localities-show.blade.php
+++ resources/views/front/statistics/region/localities-show.blade.php
@@ -7,6 +7,8 @@
         <h1>Localities in {{ $region->name }}</h1>
       </div>
 
+      <div id="map" style="width: 100%; height: 500px;"></div>
+
       <table class="table table-striped">
         <thead>
           <tr>
@@ -30,4 +32,34 @@
         </tbody>
       </table>
 </div>
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA41cF0sttrkX2sC2iwpBp5cyr6aFAIKJM&callback=initMap"
+  type="text/javascript"></script>
+<script>
+    
+    initMap = function(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+          lat: 41.865545, 
+          lng: 27.966174
+        },
+        scrollwheel: false,
+        zoom: 8
+      });
+
+    @foreach($region->localities as $locality)
+      @if($locality->latitude && $locality->longitude)
+      new google.maps.Marker({
+        position: {
+            lat: {{$locality->latitude}}, 
+            lng: {{$locality->longitude}}
+          },
+        map: map,
+        title: '{{$locality->name}}'
+      });
+      @endif
+    @endforeach
+
+  }
+  </script>
 @endsection
