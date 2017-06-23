10572a948cf37790b03ee82f405199c28c7618f1
drdimitrov <info@nortiena.com>
2017-06-23 11:44:16 +0000

    download statistic improvements

--- app/DownloadStatistic.php
+++ app/DownloadStatistic.php
@@ -17,4 +17,22 @@ class DownloadStatistic extends Model
     public function user(){
     	return $this->belongsTo(User::class);
     }
+
+    public function locality(){
+    	return $this->belongsTo(Locality::class);
+    }
+
+    public function country(){
+    	return $this->belongsTo(Country::class);
+    }
+
+    public function region(){
+    	return $this->belongsTo(Region::class);
+    }
+
+    public function getPlace(){
+    	if($this->country) return $this->country;
+    	if($this->region) return $this->region;
+    	return $this->locality;
+    }
 }
