a1c897aaab95da297d67bc969fb0b8ccb8b2b3a5
drdimitrov <info@nortiena.com>
2017-07-03 12:19:41 +0000

    species view

--- resources/views/front/species.blade.php
+++ resources/views/front/species.blade.php
@@ -13,14 +13,16 @@
             <th>Nr.</th>
             <th>Species</th>
             <th>Author</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
             @foreach($genus->species as $species)
                 <tr>
                     <td>{{ $cnt }}</td>
-                    <td><a href="{{ route('species', $species->id) }}">{{ $species->name }}</a></td>
+                    <td>{{ $species->name }}</td>
                     <td>{{ $species->author }}</td>                    
+                    <td><a href="{{ route('species', $species->id) }}">Show details</a></td>                    
                 </tr>
                 @php($cnt++)
             @endforeach          
