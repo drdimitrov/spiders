cd35946d6eb59af621bf01a0f01762b9a76408b3
vagrant <vagrant@homestead>
2017-08-12 10:45:24 +0000

    auth links color

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,7 +16,7 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS', true);
+            return redirect()->secure($request->getRequestUri());
         }
 
         return $next($request);
