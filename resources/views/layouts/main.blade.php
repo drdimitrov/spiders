8231733c889bb046e09e7be35a9dd612a07ccbbc
drdimitrov <info@nortiena.com>
2017-08-14 07:34:02 +0000

    user guide added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -37,8 +37,9 @@
           <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
               <li class="active"><a href="/">Introduction</a></li>
+              <li><a href="#" onclick="event.preventDefault(); alert('To follow.');">User guide</a></li>
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Identification <span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Taxa <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                   <li><a href="{{route('taxon.search')}}">Search for taxon</a></li>
                   <li><a href="{{route('families')}}">List all families</a></li>
