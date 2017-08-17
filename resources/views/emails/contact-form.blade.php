429add41bd711549f63300c58dd1df10c894e0f5
drdimitrov <info@nortiena.com>
2017-08-17 15:39:29 +0000

    contact form changes

--- resources/views/emails/contact-form.blade.php
+++ resources/views/emails/contact-form.blade.php
@@ -1,12 +1,6 @@
-@component('mail::message')
-# Contact form message
-
 Message from Balkan spiders contact form <br>
 
 by {{ $name }} ({{ $email }}) <br>
 Subject: {{ $subject }} <br><br>
 
 Message: {{ $body }}
-
-
-@endcomponent
