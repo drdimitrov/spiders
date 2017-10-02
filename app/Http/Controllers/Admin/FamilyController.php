3ab623b8488ecdce9adce6e657cbde5990a89c87
vagrant <vagrant@homestead>
2017-10-02 17:01:50 +0000

    family crete fix

--- app/Http/Controllers/Admin/FamilyController.php
+++ app/Http/Controllers/Admin/FamilyController.php
@@ -33,7 +33,7 @@ class FamilyController extends Controller
         $this->validate($request, ['wsc_lsid' => 'required']);
 
         $family = $wsc->fetchFamily($request->wsc_lsid);
-dd($family);
+
         if($family->taxon->status != 'VALID'){
 
             // Fetch the valid one
