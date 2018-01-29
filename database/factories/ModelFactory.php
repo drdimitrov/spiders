5c763c0780140a093239c0f6607153a034dd0221
Драгомир Димитров <info@nortiena.com>
2018-01-29 11:18:48 +0200

    Complexes factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,62 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+//$factory->define(App\User::class, function (Faker\Generator $faker) {
+//    static $password;
+//
+//    return [
+//        'name' => $faker->name,
+//        'email' => $faker->unique()->safeEmail,
+//        'password' => $password ?: $password = bcrypt('secret'),
+//        'remember_token' => str_random(10),
+//    ];
+//});
+
+$factory->define(App\Complex::class, function (Faker\Generator $faker) {
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'name' => 'North Holarctic',
+    ];
+});
+
+$factory->define(App\Complex::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => 'Siberian',
+    ];
+});
+
+$factory->define(App\Complex::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => 'European',
+    ];
+});
+
+$factory->define(App\Complex::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => 'Euro-Asian ?',
+    ];
+});
+
+$factory->define(App\Complex::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => 'Southwestern Asian',
+    ];
+});
+
+$factory->define(App\Complex::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => 'Mediterranean',
+    ];
+});
+
+$factory->define(App\Complex::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => 'Endemics',
     ];
 });
