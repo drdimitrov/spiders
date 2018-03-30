edc4d546245ec8e3689557dd63d8fbb79db414b5
Драгомир Димитров <info@nortiena.com>
2018-03-30 11:28:44 +0300

    update

--- app/Http/Controllers/Statistics/StatisticByRegionController.php
+++ app/Http/Controllers/Statistics/StatisticByRegionController.php
@@ -150,7 +150,7 @@ class StatisticByRegionController extends Controller
 
                             if(!$l['males'] && !$l['females'] && !$juvs){
                                 $l['males'] = 1;
-                                $l['juveniles'] = '';
+                                $juvs = null;
                             }
 
                             $cnt++;
