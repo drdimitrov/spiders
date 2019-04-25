a3d9eb2ad64ba7e2a2966edf451b2d1418874be1
Dragomir Dimitrov <vagrant@homestead>
2019-04-25 11:19:18 +0000

    fancybox

--- resources/views/front/single-species.blade.php
+++ resources/views/front/single-species.blade.php
@@ -109,10 +109,13 @@
 			<h4>Images:</h4>
 			@if(count($species->images))
 				@foreach($species->images as $image)
-				<a class="single_image" style="width: 160px; display: inline-block;" class="sp_img" title="{{ $species->genus->name }} {{ $species->name }}">
-					<img src="/storage/species/{{ $image->name  }}" width="150">
+				<div>
+					<a class="single_image" style="width: 160px; display: inline-block;" class="sp_img" title="{{ $species->genus->name }} {{ $species->name }}">
+						<img src="/storage/species/{{ $image->name  }}" width="150">
+					</a>
+
 					<p style="text-align: center;">{{$image->description}}</p>
-				</a>
+				</div>
 				@endforeach
 			@else
 			<p>No images available yet.</p>
