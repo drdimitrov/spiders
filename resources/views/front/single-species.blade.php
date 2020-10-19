4ad85ec2b7b2fe306b0de5f71ed50ce0b329549b
Dragomir Dimitrov <info@nortiena.com>
2020-10-19 11:15:01 +0000

    scroll top fix

--- resources/views/front/single-species.blade.php
+++ resources/views/front/single-species.blade.php
@@ -164,7 +164,7 @@
 @endsection
 
 @push('custom-scripts')
-     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+{{--     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>--}}
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.6/jquery.fancybox.min.js"></script>
 	<script>
 		$('.single_image').fancybox({
